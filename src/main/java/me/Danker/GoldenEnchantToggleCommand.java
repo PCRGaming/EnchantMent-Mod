41c372adb17549f27e88e5e8ab5a832ee429dd18
bowser0000 <bowser0000@gmail.com>
2020-10-22 11:32:56 -0400

    Initial commit

---
+++ src/main/java/me/Danker/GoldenEnchantToggleCommand.java
@@ -0,0 +1,36 @@
+package me.Danker;
+
+import net.minecraft.command.CommandBase;
+import net.minecraft.command.CommandException;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.EnumChatFormatting;
+
+public class GoldenEnchantToggleCommand extends CommandBase {
+
+	public static boolean goldToggled = true;
+	
+	@Override
+	public String getCommandName() {
+		return "goldenenchants";
+	}
+
+	@Override
+	public String getCommandUsage(ICommandSender arg0) {
+		return null;
+	}
+	
+	@Override
+	public int getRequiredPermissionLevel() {
+		return 0;
+	}
+
+	@Override
+	public void processCommand(ICommandSender arg0, String[] arg1) throws CommandException {
+		EntityPlayer player = (EntityPlayer) arg0;
+		goldToggled = !goldToggled;
+		player.addChatMessage(new ChatComponentText(EnumChatFormatting.GREEN + "Golden enchants have been set to " + EnumChatFormatting.DARK_GREEN + goldToggled));
+	}
+
+}
---
+++ src/main/java/me/Danker/TheMod.java
@@ -0,0 +1,97 @@
+package me.Danker;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.client.ClientCommandHandler;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.player.ItemTooltipEvent;
+import net.minecraftforge.fml.common.FMLCommonHandler;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.EventPriority;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+@Mod(modid = TheMod.MODID, version = TheMod.VERSION, clientSideOnly = true)
+public class TheMod {
+	public static final String MODID = "Golden Enchant Mod";
+    public static final String VERSION = "1.0";
+    
+    static Map<String, String> t6Enchants = new HashMap<String, String>();
+    static Pattern pattern = Pattern.compile("");
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+    	FMLCommonHandler.instance().bus().register(this);
+    	MinecraftForge.EVENT_BUS.register(this);
+    	
+    	t6Enchants.put("9Angler VI", "6Angler VI");
+		t6Enchants.put("9Bane of Arthropods VI", "6Bane of Arthropods VI");
+		t6Enchants.put("9Caster VI", "6Caster VI");
+		t6Enchants.put("9Compact X", "6Compact X");
+		t6Enchants.put("9Critical VI", "6Critical VI");
+		t6Enchants.put("9Dragon Hunter V", "6Dragon Hunter V");
+		t6Enchants.put("9Efficiency VI", "6Efficiency VI");
+		t6Enchants.put("9Ender Slayer VI", "6Ender Slayer VI");
+		t6Enchants.put("9Experience IV", "6Experience IV");
+		t6Enchants.put("9Expertise X", "6Expertise X");
+		t6Enchants.put("9Feather Falling X", "6Feather Falling X");
+		t6Enchants.put("9Frail VI", "6Frail VI");
+		t6Enchants.put("9Giant Killer VI", "6Giant Killer VI");
+		t6Enchants.put("9Growth VI", "6Growth VI");
+		t6Enchants.put("9Infinite Quiver X", "6Infinite Quiver X");
+		t6Enchants.put("9Lethality VI", "6Lethality VI"); 
+		t6Enchants.put("9Life Steal IV", "6Life Steal IV");
+		t6Enchants.put("9Looting IV", "6Looting IV");
+		t6Enchants.put("9Luck VI", "6Luck VI");
+		t6Enchants.put("9Luck of the Sea VI", "6Luck of the Sea VI");
+		t6Enchants.put("9Lure VI", "6Lure VI");
+		t6Enchants.put("9Magnet VI", "6Magnet VI");
+		t6Enchants.put("9Overload V", "6Overload V");
+		t6Enchants.put("9Power VI", "6Power VI");
+		t6Enchants.put("9Protection VI", "6Protection VI");
+		t6Enchants.put("9Scavenger IV", "6Scavenger IV");
+		t6Enchants.put("9Scavenger V", "6Scavenger V");
+		t6Enchants.put("9Sharpness VI", "6Sharpness VI");
+		t6Enchants.put("9Smite VI", "6Smite VI");
+		t6Enchants.put("9Spiked Hook VI", "6Spiked Hook VI");
+		t6Enchants.put("9Thunderlord VI", "6Thunderlord VI");
+		t6Enchants.put("9Vampirism VI", "6Vampirism VI");
+		
+		String patternString = "(" + String.join("|", t6Enchants.keySet()) + ")";
+		pattern = Pattern.compile(patternString);
+    }
+    
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+    	ClientCommandHandler.instance.registerCommand(new GoldenEnchantToggleCommand());
+    }
+    
+    @SubscribeEvent(priority = EventPriority.HIGHEST)
+    public void onTooltip(ItemTooltipEvent event) {
+    	if (event.toolTip == null || !GoldenEnchantToggleCommand.goldToggled) return;
+    	
+    	ItemStack item = event.itemStack;
+    	for (int i = 0; i < event.toolTip.size(); i++) {
+    		event.toolTip.set(i, returnGoldenEnchants(event.toolTip.get(i)));
+    	}
+    }
+    
+    public String returnGoldenEnchants(String line) {
+    	Matcher matcher = TheMod.pattern.matcher(line);
+    	StringBuffer out = new StringBuffer();
+    	
+    	while (matcher.find()) {
+    		matcher.appendReplacement(out, TheMod.t6Enchants.get(matcher.group(1)));
+    	}
+    	matcher.appendTail(out);
+    	
+    	return out.toString();
+    }
+    
+}
