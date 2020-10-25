683f47984109a3c75e8be9ab8e57cb80ae571d79
PCRGaming <65746213+PCRGaming@users.noreply.github.com>
2020-10-25 16:18:11 +1100

    Update TheMod.java

--- src/main/java/me/Danker/TheMod.java
+++ src/main/java/me/Danker/TheMod.java
@@ -19,7 +19,7 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @Mod(modid = TheMod.MODID, version = TheMod.VERSION, clientSideOnly = true)
 public class TheMod {
-	public static final String MODID = "Golden Enchant Mod";
+	public static final String MODID = "EnchantMend Mod";
     public static final String VERSION = "1.0";
     
     static Map<String, String> t6Enchants = new HashMap<String, String>();
