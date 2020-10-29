1b020e9e7eb03e3b25c81ec2288c6aabd31d226e
PCRGaming <proboscowong36@gmail.com>
2020-10-29 21:22:45 +1100

    Just testing

--- src/main/java/me/Danker/TheMod.java
+++ src/main/java/me/Danker/TheMod.java
@@ -21,16 +21,16 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 public class TheMod {
 	public static final String MODID = "EnchantMent Mod";
     public static final String VERSION = "1.0";
-    
+
     static Map<String, String> t6Enchants = new HashMap<String, String>();
     static Pattern pattern = Pattern.compile("");
-    
+
     @EventHandler
     public void init(FMLInitializationEvent event) {
     	FMLCommonHandler.instance().bus().register(this);
     	MinecraftForge.EVENT_BUS.register(this);
-    	
-    	t6Enchants.put("9Angler VI", "6Angler VI");
+
+  t6Enchants.put("9Angler VI", "6Angler VI");
 	t6Enchants.put("9Bane of Arthropods VI", "6Bane of Arthropods VI");
 	t6Enchants.put("9Big Brain V","6Big Brain V");
 	t6Enchants.put("9Caster VI", "6Caster VI");
@@ -49,7 +49,7 @@ public class TheMod {
 	t6Enchants.put("9Growth VI", "6Growth VI");
 	t6Enchants.put("9Growth VII","4Growth VII");
 	t6Enchants.put("9Infinite Quiver X", "4Infinite Quiver X");
-	t6Enchants.put("9Lethality VI", "6Lethality VI"); 
+	t6Enchants.put("9Lethality VI", "6Lethality VI");
 	t6Enchants.put("9Life Steal IV", "6Life Steal IV");
 	t6Enchants.put("9Looting IV", "6Looting IV");
 	t6Enchants.put("9Luck VI", "6Luck VI");
@@ -63,44 +63,44 @@ public class TheMod {
 	t6Enchants.put("9Protection VII","4Protection VII");
 	t6Enchants.put("9Scavenger IV", "6Scavenger IV");
 	t6Enchants.put("9Scavenger V", "4Scavenger V");
-	t6Enchants.put("9Sharpness VII","4Sharpness VII");
+	t6Enchants.put("6Sharpness VII","4Sharpness VII");
 	t6Enchants.put("9Sharpness VI", "6Sharpness VI");
-	t6Enchants.put("9Smite VII","4Smite VII");
+	t6Enchants.put("6Smite VII","4Smite VII");
 	t6Enchants.put("9Smite VI", "6Smite VI");
 	t6Enchants.put("9Spiked Hook VI", "6Spiked Hook VI");
 	t6Enchants.put("9Thunderlord VI", "6Thunderlord VI");
 	t6Enchants.put("9Vampirism VI", "6Vampirism VI");
 	t6Enchants.put("9Vicious V","6Vicious V");
-		
+
 		String patternString = "(" + String.join("|", t6Enchants.keySet()) + ")";
 		pattern = Pattern.compile(patternString);
     }
-    
+
     @EventHandler
     public void preInit(FMLPreInitializationEvent event) {
     	ClientCommandHandler.instance.registerCommand(new GoldenEnchantToggleCommand());
     }
-    
+
     @SubscribeEvent(priority = EventPriority.HIGHEST)
     public void onTooltip(ItemTooltipEvent event) {
     	if (event.toolTip == null || !GoldenEnchantToggleCommand.goldToggled) return;
-    	
+
     	ItemStack item = event.itemStack;
     	for (int i = 0; i < event.toolTip.size(); i++) {
     		event.toolTip.set(i, returnGoldenEnchants(event.toolTip.get(i)));
     	}
     }
-    
+
     public String returnGoldenEnchants(String line) {
     	Matcher matcher = TheMod.pattern.matcher(line);
     	StringBuffer out = new StringBuffer();
-    	
+
     	while (matcher.find()) {
     		matcher.appendReplacement(out, TheMod.t6Enchants.get(matcher.group(1)));
     	}
     	matcher.appendTail(out);
-    	
+
     	return out.toString();
     }
-    
+
 }
