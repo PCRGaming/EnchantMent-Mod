22cecb3b8626cd41b1aad8f8844ce4d64cb87c40
PCRGaming <65746213+PCRGaming@users.noreply.github.com>
2020-11-02 10:23:12 +1100

    Update TheMod.java

--- src/main/java/me/Danker/TheMod.java
+++ src/main/java/me/Danker/TheMod.java
@@ -1,4 +1,4 @@
-package me.Danker;
+package me.Danker; //Alexander Tran
 
 import java.util.HashMap;
 import java.util.Map;
@@ -30,53 +30,52 @@ public class TheMod {
     	FMLCommonHandler.instance().bus().register(this);
     	MinecraftForge.EVENT_BUS.register(this);
 //T10 Enchants (Red)
-t6Enchants.put("9Compact X", "4Compact X");
+t6Enchants.put("9Compact X", "4Compact X"); //1mil ores required
 t6Enchants.put("9Expertise X", "4Expertise X");
 t6Enchants.put("9Feather Falling X", "4Feather Falling X");
 t6Enchants.put("9Infinite Quiver X", "4Infinite Quiver X");
 //T7 Enchants (Red)
 t6Enchants.put("9Giant Killer VII","4Giant Killer VII");
 t6Enchants.put("9Growth VII","4Growth VII");
+t6Enchants.put("9Power VII","4Power VII");
+t6Enchants.put("9Protection VII","4Protection VII");
+t6Enchants.put("9Sharpness VII","4Sharpness VII");
+t6Enchants.put("9Smite VII","4Smite VII");
 //T6 Enchants (Gold)
+t6Enchants.put("9Angler VI", "6Angler VI");
+t6Enchants.put("9Bane of Arthropods VI", "6Bane of Arthropods VI");
+t6Enchants.put("9Caster VI", "6Caster VI");
+t6Enchants.put("9Critical VI", "6Critical VI");
+t6Enchants.put("9Efficiency VI", "6Efficiency VI");
+t6Enchants.put("9Ender Slayer VI", "6Ender Slayer VI");
+t6Enchants.put("9Frail VI", "6Frail VI");
+t6Enchants.put("9Giant Killer VI", "6Giant Killer VI");
+t6Enchants.put("9Growth VI", "6Growth VI");
+t6Enchants.put("9Lethality VI", "6Lethality VI");
+t6Enchants.put("9Luck VI", "6Luck VI");
+t6Enchants.put("9Luck of the Sea VI", "6Luck of the Sea VI");
+t6Enchants.put("9Lure VI", "6Lure VI");
+t6Enchants.put("9Magnet VI", "6Magnet VI");
+t6Enchants.put("9Power VI", "6Power VI");
+t6Enchants.put("9Protection VI", "6Protection VI");
+t6Enchants.put("9Sharpness VI", "6Sharpness VI");
+t6Enchants.put("9Smite VI", "6Smite VI");
+t6Enchants.put("9Spiked Hook VI", "6Spiked Hook VI");
+t6Enchants.put("9Thunderlord VI", "6Thunderlord VI");
+t6Enchants.put("9Vampirism VI", "6Vampirism VI");
 //T5 Enchants (Gold)
+t6Enchants.put("9Big Brain V","6Big Brain V");
+t6Enchants.put("9Counter-Strike V","6Counter-Strike V");
+t6Enchants.put("9Dragon Hunter V", "6Dragon Hunter V");
+t6Enchants.put("9Overload V", "6Overload V");
+t6Enchants.put("9Scavenger V", "4Scavenger V");
+t6Enchants.put("9Vicious V","6Vicious V");
 //T4 Enchants (Gold)
+t6Enchants.put("9Experience IV", "6Experience IV");
+t6Enchants.put("9Life Steal IV", "6Life Steal IV");
+t6Enchants.put("9Looting IV", "6Looting IV");
+t6Enchants.put("9Scavenger IV", "6Scavenger IV");
 //T1 Enchants (Color)
-  t6Enchants.put("9Angler VI", "6Angler VI");
-	t6Enchants.put("9Bane of Arthropods VI", "6Bane of Arthropods VI");
-	t6Enchants.put("9Big Brain V","6Big Brain V");
-	t6Enchants.put("9Caster VI", "6Caster VI");
-	t6Enchants.put("9Counter-Strike V","6Counter-Strike V");
-	t6Enchants.put("9Critical VI", "6Critical VI");
-	t6Enchants.put("9Dragon Hunter V", "6Dragon Hunter V");
-	t6Enchants.put("9Efficiency VI", "6Efficiency VI");
-	t6Enchants.put("9Ender Slayer VI", "6Ender Slayer VI");
-	t6Enchants.put("9Experience IV", "6Experience IV");
-	t6Enchants.put("9Frail VI", "6Frail VI");
-	t6Enchants.put("9Giant Killer VI", "6Giant Killer VI");
-	t6Enchants.put("9Growth VI", "6Growth VI");
-	t6Enchants.put("9Lethality VI", "6Lethality VI");
-	t6Enchants.put("9Life Steal IV", "6Life Steal IV");
-	t6Enchants.put("9Looting IV", "6Looting IV");
-	t6Enchants.put("9Luck VI", "6Luck VI");
-	t6Enchants.put("9Luck of the Sea VI", "6Luck of the Sea VI");
-	t6Enchants.put("9Lure VI", "6Lure VI");
-	t6Enchants.put("9Magnet VI", "6Magnet VI");
-	t6Enchants.put("9Overload V", "6Overload V");
-	t6Enchants.put("9Power VI", "6Power VI");
-	t6Enchants.put("9Power VII","4Power VII");
-	t6Enchants.put("9Protection VI", "6Protection VI");
-	t6Enchants.put("9Protection VII","4Protection VII");
-	t6Enchants.put("9Scavenger IV", "6Scavenger IV");
-	t6Enchants.put("9Scavenger V", "4Scavenger V");
-	t6Enchants.put("9Sharpness VII","4Sharpness VII");
-	t6Enchants.put("9Sharpness VI", "6Sharpness VI");
-	t6Enchants.put("9Smite VII","4Smite VII");
-	t6Enchants.put("9Smite VI", "6Smite VI");
-	t6Enchants.put("9Spiked Hook VI", "6Spiked Hook VI");
-	t6Enchants.put("9Thunderlord VI", "6Thunderlord VI");
-	t6Enchants.put("9Vampirism VI", "6Vampirism VI");
-	t6Enchants.put("9Vicious V","6Vicious V");
-
 		String patternString = "(" + String.join("|", t6Enchants.keySet()) + ")";
 		pattern = Pattern.compile(patternString);
     }
